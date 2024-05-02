19bf07c9a1f3235d8992f9ef4fb8f12078d7bd08
olyshaaa <olgasevel108@gmail.com>
2024-05-02 18:34:50 +0300

    landing page

---
+++ script.js
@@ -0,0 +1,18 @@
+new Swiper(".mySwiper", {
+	spaceBetween: 30,
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	},
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: true,
+	},
+	keyboard: true,
+	loop: true,
+	observer: true,
+    observeParents: true
+  });
